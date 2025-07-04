2f513f0364098d631e8b41fb6b19cb836704ed39
DucAnh9981 <anhnguyeduc331@gmail.com>
2025-07-04 07:08:11 +0700

    update/update-func-request-confirm-service-and-activity-log

--- src/modules/notification/notification.controller.ts
+++ src/modules/notification/notification.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post, Body, Param, Delete, Query, UseGuards, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Query,
+  UseGuards,
+  Patch,
+} from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { CreateNotificationDto } from './dto/create-notification.dto';
-import { JwtAuth } from 'src/common/decorators/jwt-auth.decorator'; 
+import { JwtAuth } from 'src/common/decorators/jwt-auth.decorator';
 import { User } from 'src/common/decorators/current-user.decorator';
 import { JwtPayload } from 'src/common/interfaces/jwt-payload.interface';
 import { ApiTags } from '@nestjs/swagger';
@@ -27,27 +37,28 @@ export class NotificationController {
   }
 
   @Get('unread/count')
-  async getUnreadCount(@User() user: JwtPayload,) {
+  async getUnreadCount(@User() user: JwtPayload) {
     return await this.notificationService.getUnreadCount(user.id);
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string, @User() user: JwtPayload,) {
+  async findOne(@Param('id') id: string, @User() user: JwtPayload) {
     return await this.notificationService.findOne(id, user.id);
   }
 
   @Patch(':id/read')
-  async markAsRead(@Param('id') id: string, @User() user: JwtPayload,) {
+  @JwtAuth()
+  async markAsRead(@Param('id') id: string, @User() user: JwtPayload) {
     return await this.notificationService.markAsRead(id, user.id);
   }
 
   @Patch('read-all')
-  async markAllAsRead(@User() user: JwtPayload,) {
+  async markAllAsRead(@User() user: JwtPayload) {
     return await this.notificationService.markAllAsRead(user.id);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string, @User() user: JwtPayload,) {
+  async remove(@Param('id') id: string, @User() user: JwtPayload) {
     return await this.notificationService.remove(id, user.id);
   }
-} 
+}
